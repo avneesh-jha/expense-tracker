d24cfc5036b13f778015968a4e46f35a6d688a98
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-09 23:18:57 +0530

    feat:useDispatch and action for writing in store

--- src/components/ExpenseInput/ExpenseInput.jsx
+++ src/components/ExpenseInput/ExpenseInput.jsx
@@ -1,8 +1,19 @@
+import { useDispatch } from "react-redux";
 import s from "./style.module.css";
+import { addExpenseAction } from "store/expense/expense-slice";
+import { useState } from "react";
 
 export function ExpenseInput(props) {
+  const dispatch = useDispatch();
+  const [price, setPrice] = useState();
+  const [expenseName, setExpenseName] = useState();
+  function submit(e) {
+    e.preventDefault();
+    dispatch(addExpenseAction({ expenseName, price }));
+    console.log("events", e);
+  }
   return (
-    <form>
+    <form onSubmit={submit}>
       <div className="row justify-content-center">
         <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
           <input
@@ -10,6 +21,7 @@ export function ExpenseInput(props) {
             className="form-control"
             placeholder='Ex : "Apple"'
             name="name"
+            onChange={(e) => setExpenseName(e.target.value)}
           />
         </div>
         <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
@@ -19,6 +31,7 @@ export function ExpenseInput(props) {
             className="form-control"
             placeholder="Ex: 3.99"
             name="price"
+            onChange={(e) => setPrice(e.target.value)}
           />
         </div>
 
